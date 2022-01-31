d3e568923fbd464e0181cae629db493537bab798
gilgamesh7 <felidaesrule@yahoo.com>
2022-01-31 14:26:55 +1300

    Added finish & move 1 player to finish line

--- app.py
+++ app.py
@@ -32,19 +32,32 @@ def setup_game()->None:
         race_surface = pygame.Surface((1200,600))
         race_surface.fill('Green')
 
+        # Set up race surface / background
         player_surface = pygame.Surface((50,50))
         player_surface.fill('Red')
         player_rect = player_surface.get_rect(center=(10,10))
 
-        return race_surface, player_surface, player_rect
+        # Set up timer
+        movement_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(movement_timer, 2000)
+
+        # Caption
+        game_font = pygame.font.SysFont('timesnewroman',  50)
+        race_finish_surface = game_font.render('FINISH', False, (64, 64, 64))
+        race_finish_surface = pygame.transform.rotozoom(race_finish_surface, 270, 3)
+
+
+        return race_surface, race_finish_surface, movement_timer, player_surface, player_rect
     except Exception as err:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logger.info(f"{err} on line {exc_tb.tb_lineno}")
         raise err
 
-def game_loop(screen: object, clock: object, race_surface: object, player_surface:object, player_rect:object)->None:
+def game_loop(screen: object, clock: object, movement_timer:object, race_surface: object, race_finish_surface: object, player_surface:object, player_rect:object)->None:
     try:
+        player_x_pos = 0
         while True:
+            print(player_x_pos)
             # check for events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -54,15 +67,21 @@ def game_loop(screen: object, clock: object, race_surface: object, player_surfac
                     # Ensure pygame ends 
                     exit()
 
+            # if event.type == movement_timer:
+            if player_rect.x < 950 :
+                player_x_pos += 1
+                player_rect.x += player_x_pos
+
             # blit - Blcok Image Transfer i.e put a regular surface on top of display surface
             screen.blit(race_surface,(0,0))
+            screen.blit(race_finish_surface,(1000,50))
             screen.blit(player_surface, player_rect)
 
             # Writeupdates to display surface
             pygame.display.update()
 
             # Control speed , set maximum frame rate - while loop must not run more than 60 times per second
-            clock.tick(60)
+            clock.tick(10)
     except Exception as err:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logger.info(f"{err} on line {exc_tb.tb_lineno}")
@@ -73,9 +92,9 @@ def main()->None:
 
     screen, clock = setup_pygame()
 
-    race_surface, player_surface, player_rect = setup_game()
+    race_surface, race_finish_surface, movement_timer, player_surface, player_rect = setup_game()
 
-    game_loop(screen, clock, race_surface, player_surface, player_rect)
+    game_loop(screen, clock, movement_timer, race_surface, race_finish_surface, player_surface, player_rect)
 
 
 if __name__ == "__main__":
